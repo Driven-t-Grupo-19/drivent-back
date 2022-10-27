c3de72d282b96dd57898c55adc57cc916bd06bfb
seu nome <adnanbezerra@gmail.com>
2022-10-27 17:25:42 -0300

    refactor: remove isOccupied from Accommodations

--- prisma/migrations/20221027202119_help_please/migration.sql
+++ prisma/migrations/20221027202119_help_please/migration.sql
@@ -1,6 +1,2 @@
--- AlterTable
-ALTER TABLE "accommodations" ADD COLUMN     "slots" INTEGER NOT NULL DEFAULT 1,
-ADD COLUMN     "userId" INTEGER NOT NULL DEFAULT 0;
-
 -- AddForeignKey
 ALTER TABLE "accommodations" ADD CONSTRAINT "accommodations_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
