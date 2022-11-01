1fc9488244156009cd38c53458fb8da3b468d9e8
seu nome <adnanbezerra@gmail.com>
2022-10-31 21:41:59 -0300

    feat: activities seeding

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -18,8 +18,7 @@ async function main() {
 
   await createHotels();
   await createAcommodations();
-
-  console.log({ event });
+  await createActivies();
 }
 
 async function createHotels() {
@@ -47,13 +46,34 @@ async function createAcommodationsByHotelId(hotelId: number) {
   await prisma.accommodations.createMany({
     data: [
       { number: 100, type: 'SINGLE', hotelId: hotelId, slots: 1 },
-      { number: 101, type: 'DOUBLE', hotelId: hotelId, slots: 1 , users:[996] },
+      { number: 101, type: 'DOUBLE', hotelId: hotelId, slots: 1, users: [996] },
       { number: 101, type: 'DOUBLE', hotelId: hotelId, slots: 2 },
-      { number: 102, type: 'TRIPLE', hotelId: hotelId, slots: 1 , users:[998, 997] },
+      { number: 102, type: 'TRIPLE', hotelId: hotelId, slots: 1, users: [998, 997] },
       { number: 102, type: 'TRIPLE', hotelId: hotelId, slots: 3 },
-      { number: 103, type: 'SINGLE', hotelId: hotelId, slots: 0 , users:[999] }
+      { number: 103, type: 'SINGLE', hotelId: hotelId, slots: 0, users: [999] }
     ]
   });
+}
+
+async function createActivies() {
+  await prisma.activities.createMany({
+    data: [
+      { name: 'Top 10 Aliens Ben 10', auditorium: 'MAIN', startsAt: '10:00', endsAt: '11:00', day: getDatePlusNDays(2) },
+      { name: 'Como zerar Skyrim', auditorium: 'SIDE', startsAt: '10:00', endsAt: '17:00', day: getDatePlusNDays(2) },
+      { name: 'Anúncio do LoL 2', auditorium: 'WORKSHOP', startsAt: '10:00', endsAt: '11:00', day: getDatePlusNDays(2) },
+      { name: 'Frank parece o Mano Walter?', auditorium: 'MAIN', startsAt: '10:00', endsAt: '11:00', day: getDatePlusNDays(3) },
+      { name: 'Top 10 gols do Gabigol', auditorium: 'SIDE', startsAt: '10:00', endsAt: '17:00', day: getDatePlusNDays(3) },
+      { name: 'Dudu parece o Kaka?', auditorium: 'WORKSHOP', startsAt: '10:00', endsAt: '11:00', day: getDatePlusNDays(3) },
+      { name: 'Aula inicial de JS com Pedrão', auditorium: 'MAIN', startsAt: '10:00', endsAt: '11:00', day: getDatePlusNDays(4) },
+      { name: 'Expondo os problemas do React', auditorium: 'SIDE', startsAt: '10:00', endsAt: '17:00', day: getDatePlusNDays(4) },
+      { name: 'Como falar em público', auditorium: 'WORKSHOP', startsAt: '10:00', endsAt: '11:00', day: getDatePlusNDays(4) }
+    ]
+  });
+}
+
+function getDatePlusNDays(n: number) {
+  const today = new Date();
+  return new Date(today.getFullYear(), today.getMonth(), today.getDate() + n);
 }
 
 main()
